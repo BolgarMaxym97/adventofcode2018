3e07f5ee7900b0f1d50864a5ead8511cdc4f7694
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-12-12 13:30:17 +0200

    fix

--- days/2/2-day-2.py
+++ days/2/2-day-2.py
@@ -2,13 +2,13 @@ from pip._vendor.msgpack.fallback import xrange
 
 f = open('./dist/2.txt', 'r').readlines()
 stringsArr = [line.rstrip() for line in f]
-previous = ''
 position = 0
-result = ''
 string = []
+
 for stringItemFirst in stringsArr:
     for j in range(1, len(stringsArr)):
         positions = [i for i in xrange(len(stringsArr[j])) if stringsArr[j][i] != stringItemFirst[i]]
+        print(positions)
         if len(positions) == 1:
             position = positions.pop()
             string = stringItemFirst
