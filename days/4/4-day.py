13cb4a5dbb4a028aee25a45881b9c283d9f63b8c
BolgarMaxym97 <bolgar.maxym@gmail.com>
2018-12-13 13:28:25 +0200

    refactored 4 day and part 2

--- days/4/4-day.py
+++ days/4/4-day.py
@@ -1,47 +1,19 @@
-import re
-from collections import defaultdict
-from dateutil import parser
+from collections import Counter, defaultdict
+from datetime import datetime
 
-WAKES_UP = 'wakes up'
-FALLS_ASLEEP = 'falls asleep'
-f = open('../../dist/4.txt', 'r').readlines()
-linesArr = [line.rstrip() for line in f]
-dictionary = sorted({re.search('\[(.*)]', item).group(1): item.split(']')[1][1:] for item in linesArr}.items())
-guardID = 0
-infoByGuards = defaultdict(list)
-for date, value in dictionary:
-    if value != WAKES_UP and value != FALLS_ASLEEP:
-        guardID = ''.join(c for c in value if c.isdigit())
-    elif value == FALLS_ASLEEP:
-        infoByGuards[guardID].append({'sleepTime': date})
-    else:
-        infoByGuards[guardID].append({'wakesTime': date})
-countingByGuards = defaultdict(list)
-for guardID, values in infoByGuards.items():
-    startSleep = ''
-    endSleep = ''
-    minutes = 0
-    minutesSum = 0
-    maxMin = 0
-    for action in values:
-        parsed = [item for item in action.items()].pop()
-        actionName = parsed[0]
-        date = parsed[1]
-        if  actionName == 'sleepTime':
-            startSleep = parser.parse(date)
-        else:
-            minutes = int((parser.parse(date) - startSleep).seconds / 60)
-        minutesSum += minutes
-        if maxMin < minutes:
-            maxMin = minutes
-    countingByGuards[guardID].append([minutesSum, maxMin])
+guards = defaultdict(Counter)
+for t, m in [l.split('] ') for l in sorted(open('../../dist/4.txt', 'r').readlines()) if l]:
+    t = datetime.strptime(t, '[%Y-%m-%d %H:%M')
+    if '#' in m:     g = int(m.split('#')[1].split()[0])
+    if 'falls' in m: start = t
+    if 'wakes' in m:
+        minutes = int((t - start).total_seconds() // 60)
+        guards[g].update(Counter((start.minute+i)%60 for i in range(minutes)))
 
-maxGuardId = 0
-maxSleep = 0
-for guardID, data in countingByGuards.items():
-    if maxSleep < data[0][0]:
-        maxGuardId = guardID
-maxTimeResult = countingByGuards[maxGuardId].pop()[1]
-print(int(maxGuardId))
-print(int(maxGuardId) * int(maxTimeResult) - 1)
+_, id = max((sum(c.values()), id) for id, c in guards.items())
+part1 = id * guards[id].most_common()[0][0]
 
+(_, minute), id = max((c.most_common()[0][::-1], id) for id, c in guards.items())
+part2 = id * minute
+
+print(part1, part2)
